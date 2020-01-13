7299f3940fa7e497c1955fccdd0fd4bfc96e82fd
ivan miller <ivanmcardoso@hotmail.com>
2020-01-12 22:01:46 -0400

    first commit

---
+++ src/main/java/com/gear/proex/ProexApplication.java
@@ -0,0 +1,13 @@
+package com.gear.proex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gear/proex/ProexApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gear.proex;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProexApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
