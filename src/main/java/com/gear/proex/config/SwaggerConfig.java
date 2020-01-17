2054199d583ec1336db8321b84058430a1e984ed
Ivan Miller Cardoso <ivanmcardoso@hotmail.com>
2020-01-17 13:43:58 -0400

    Update SwaggerConfig.java
    config example

--- src/main/java/com/gear/proex/config/SwaggerConfig.java
+++ src/main/java/com/gear/proex/config/SwaggerConfig.java
@@ -29,5 +29,85 @@ public class SwaggerConfig {
                 .description("Documentação da API de acesso aos endpoints do Proex.").version("1.0")
                 .build();
     }
+    
+    
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).forCodeGeneration(true)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .select()
+                .apis(apis())
+                .paths(PathSelectors.any())
+                .build().securitySchemes(Collections.singletonList(securitySchema()))
+                .apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()));
+
+    }
+
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
+        authorizationScopes[0] = new AuthorizationScope("read", "read all");
+        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
+        authorizationScopes[2] = new AuthorizationScope("write", "write all");
+
+        return Collections.singletonList(new SecurityReference("oauth2schema", authorizationScopes));
+
+    }
+
+    private Predicate<RequestHandler> apis() {
+        return RequestHandlerSelectors.basePackage("br.com.itbam.controller");
+    }
+
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfos = new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .version(VERSION)
+                .build();
+        return apiInfos;
+    }
+
+    private OAuth securitySchema() {
+
+        List<AuthorizationScope> authorizationScopeList = new ArrayList();
+        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+        authorizationScopeList.add(new AuthorizationScope("trust", "trust all"));
+        authorizationScopeList.add(new AuthorizationScope("write", "access all"));
+
+        List<GrantType> grantTypes = new ArrayList();
+
+        grantTypes.add(new ResourceOwnerPasswordCredentialsGrant(tokenUrl));
+
+        return new OAuth("oauth2schema", authorizationScopeList, grantTypes);
+
+    }
+
+    @Bean
+    public SecurityConfiguration securityInfo() {
+        SecurityConfigurationBuilder builder = SecurityConfigurationBuilder.builder();
+        builder.clientId(pdmApiProperty.getSeguranca().getSwaggerClientId());
+        builder.clientSecret(clientSecret);
+        builder.scopeSeparator(" ");
+
+        return builder.build();
+    }
+
 
 }
